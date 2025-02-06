3aefb9fcb4edb0957b5462854770f090de63b245
sungang@china-alert.com <sungang@china-alert.com>
2025-02-06 17:42:07 +0800

    删除测试代码

--- ueh_probe/pprobe/src/main/java/com/mcinfotech/event/probe/config/DataSourceConfig.java
+++ ueh_probe/pprobe/src/main/java/com/mcinfotech/event/probe/config/DataSourceConfig.java
@@ -38,8 +38,6 @@ public class DataSourceConfig {
 	
 	@Bean
 	public DataSource build() {
-		//String url="jdbc:postgresql://127.0.0.1:5432/ueh?currentSchema=ueh_ctrl&TimeZone=PRC&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT";
-		//String driverClassName="org.postgresql.Driver";
 		Properties prop=new Properties();
 		prop.put("initial.size", new Integer(initialSize).toString());
 		prop.put("max.total", new Integer(maxTotal).toString());
